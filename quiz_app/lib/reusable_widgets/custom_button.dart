b2fdac28f854f15cef1f4a6b472e895949894af9
AkshayRameshAppDEV <akshay_ramesh@yahoo.com>
2022-01-03 17:58:17 -0600

    Format

--- quiz_app/lib/reusable_widgets/custom_button.dart
+++ quiz_app/lib/reusable_widgets/custom_button.dart
@@ -6,16 +6,21 @@ class CustomButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-  child:  Container(
-    width: 300.0,
-    height: 50.0,
-    decoration:  BoxDecoration(
-      color: const Color(0xff6949fd),
-      border:  Border.all(color: Colors.white, width: 2.0),
-      borderRadius:  BorderRadius.circular(10.0),
-    ),
-    child:  const Center(child:  Text('Click Me', style:  TextStyle(fontSize: 18.0, color: Colors.white),),),
-  ),
-);
+      child: Container(
+        width: 300.0,
+        height: 50.0,
+        decoration: BoxDecoration(
+          color: const Color(0xff6949fd),
+          border: Border.all(color: Colors.white, width: 2.0),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: const Center(
+          child: Text(
+            'Click Me',
+            style: TextStyle(fontSize: 18.0, color: Colors.white),
+          ),
+        ),
+      ),
+    );
   }
 }
