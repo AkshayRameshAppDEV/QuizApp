04db5d726ab0333d692035c1be2712fee6dec02d
AkshayRameshAppDEV <akshay_ramesh@yahoo.com>
2022-01-05 10:51:14 -0600

    Add polygon clipper library

--- quiz_app/lib/screens/levels.dart
+++ quiz_app/lib/screens/levels.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../reusable_widgets/custom_text.dart';
 import 'dart:math' as math;
+import 'package:polygon_clipper/polygon_clipper.dart';
 
 class Levels extends StatelessWidget {
   const Levels({Key? key}) : super(key: key);
@@ -30,77 +31,63 @@ class Levels extends StatelessWidget {
         // Generate 100 widgets that display their index in the List.
         children: List.generate(8, (index) {
           return Center(
-            child: ClipPath(
-              clipper: MyPolygon(),
-              child: Container(
-                color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
-                    .withOpacity(0.5),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const CustomText(
-                        'Level', 25.0, Colors.white, FontWeight.bold),
-                    CustomText(
-                        '${index + 1}', 20.0, Colors.white, FontWeight.bold),
-                    Row(
+            child: Container(
+                padding: const EdgeInsets.all(30.0),
+                child: ClipPolygon(
+                  sides: 5,
+                  child: Container(
+                    color:
+                        Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
+                            .withOpacity(0.5),
+                    child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Icon(
-                          Icons.star,
-                          size: 20,
-                          color: Colors.yellow, //The color which you want set.
-                        ),
-                        Icon(
-                          Icons.star,
-                          size: 20,
-                          color: Colors.yellow, //The color which you want set.
-                        ),
-                        Icon(
-                          Icons.star,
-                          size: 20,
-                          color: Colors.yellow, //The color which you want set.
-                        ),
-                        Icon(
-                          Icons.star,
-                          size: 20,
-                          color: Colors.yellow, //The color which you want set.
-                        ),
-                        Icon(
-                          Icons.star,
-                          size: 20,
-                          color: Colors.yellow, //The color which you want set.
-                        ),
+                      children: [
+                        const CustomText(
+                            'Level', 25.0, Colors.white, FontWeight.bold),
+                        CustomText('${index + 1}', 20.0, Colors.white,
+                            FontWeight.bold),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Icon(
+                              Icons.star,
+                              size: 20,
+                              color:
+                                  Colors.yellow, //The color which you want set.
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 20,
+                              color:
+                                  Colors.yellow, //The color which you want set.
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 20,
+                              color:
+                                  Colors.yellow, //The color which you want set.
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 20,
+                              color:
+                                  Colors.yellow, //The color which you want set.
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 20,
+                              color:
+                                  Colors.yellow, //The color which you want set.
+                            ),
+                          ],
+                        )
                       ],
-                    )
-                  ],
-                ),
-                width: 150,
-                height: 150,
-              ),
-            ),
+                    ),
+                  ),
+                )),
           );
         }),
       ),
     );
-  }
-}
-
-class MyPolygon extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    Path path = Path();
-    path.addPolygon([
-      Offset(0, size.height * 1 / 3),
-      Offset(size.width / 2, 0),
-      Offset(size.width, size.height * 1 / 3),
-      Offset(size.width * 4 / 5, size.height),
-      Offset(size.width * 1 / 5, size.height),
-    ], true);
-    return path;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    return false;
   }
 }
